82d83146b3a90833cc4b215115f255ceb480badf
Viacheslav <viacheslav.vozniuk@gmail.com>
2020-02-06 15:46:17 +0200

    init: start "calculator" task

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import '@babel/polyfill';
+
+function App() {
+  return <div className="text">Lease and Loan calculator</div>;
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
